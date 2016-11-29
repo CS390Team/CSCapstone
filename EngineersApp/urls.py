b4e0d08702855f3a004ef47c35ae8851c82fe26d
shi180 <shi180@purdue.edu>
2016-11-28 22:27:43 -0500

    Add nav page

--- EngineersApp/urls.py
+++ EngineersApp/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^engineers$', views.getEngineers, name='Engineers'),
+    url(r'^engineerform$', views.getEngineerForm, name='EngineerForm'),
+    url(r'^addengineer$', views.addEngineer, name='AddEngineer'),
 ]
