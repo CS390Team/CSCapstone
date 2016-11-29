e8fe67c485803709389d0c8730cc50b8518ed23a
Hanxiao Hu <hu247@purdue.edu>
2016-11-28 21:16:27 -0500

    Add teachers model.

---
+++ TeachersApp/models.py
@@ -0,0 +1,10 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Teachser(models.Model):
+	name = models.CharField(max_length=500)
+	university = models.CharField(max_length=500)
+	email = models.CharField(max_length=500)
+	phone = models.CharField(max_length=500)
